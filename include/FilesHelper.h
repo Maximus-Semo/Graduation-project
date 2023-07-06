c35c3d93a27d23c94f86a7d2448becb4f301a5c1
Maximus <maximos.rafaat89@gmail.com>
2023-07-06 20:20:24 +0300

    Parser & FileHelper
    Parser to convert from string to original data type and return this cover as object to be,,
    Fill helper to save last id and and save data from the user and add last id form the fileLastId and some method as like pdf explan to us the method do
    (i still work on small issues !).. on the code//

--- include/FilesHelper.h
+++ include/FilesHelper.h
@@ -1,22 +1,128 @@
 #ifndef FILESHELPER_H
 #define FILESHELPER_H
-
-#include<iostream>
+#include <Parser.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 #include<string>
 
 using namespace std;
 class FilesHelper {
-
+//MTHODS -> !
 public:
-    //Methods
-   static void saveLast(string fileName, int id){};
-   static int getLast(string fileName){};
-   static void saveClient(Client c){};
-   static void saveEmployee(string fileName, string lastIdFile, Employee e){};
-   static void getClient(){};
-   static void getEmployees(){};
-   static void getAdmins(){};
-   static void clearFile(string fileName, string lastIdFile){};
+    static void saveLast(const string& enterLastIDataBase, int id){
+        ofstream MyClientLastId(enterLastIDataBase);
+        MyClientLastId<<id;
+   };
+
+   //Get Last ID form -> file:.
+   static int getLast(const string& getLastIdDataBase){
+        string line;
+        int lastId = 0;
+        ifstream getId(getLastIdDataBase);
+        if(!getId.is_open()){
+            cerr<< "Error opening file: " << getLastIdDataBase<<endl;
+            return 0;
+        }
+        getline(getId, line);
+            try {
+                int currentId = stoi(line);
+                lastId = currentId;
+            }catch (const invalid_argument& e) {
+                cerr << "Invalid argument: " << e.what() << endl;
+            }
+        return lastId;
+   };
+
+   static void saveClient(Client c){
+        ofstream MyClient("Clients.txt",ios::app);
+        string ClientLastId = "ClientLastId.txt";
+        int setLastId = getLast(ClientLastId);
+        setLastId++;
+        saveLast(ClientLastId,setLastId);
+        if(MyClient.is_open()){
+            MyClient<< setLastId <<"*"<<c.getName()<<"*"<<c.getPassword()<<"*"<<c.getBalance()<<endl;
+        } else {
+            cout <<"Field"<<endl;
+        }
+   };
+   static void saveEmployeeOrAdmin(string& fileName, string& lastIdFile, Employee& e){
+        ofstream file_Employee_Admin(fileName,ios::app);
+        int setLastId = getLast(lastIdFile);
+        setLastId++;
+        saveLast(lastIdFile,setLastId);
+        if(file_Employee_Admin.is_open()){
+            file_Employee_Admin<< setLastId <<"*"<<e.getName()<<"*"<<e.getPassword()<<"*"<<e.getSalary()<<endl;
+        }else{
+            cout <<"Field"<<endl;
+        }
+   };
+
+
+   static void getClients() {
+        ifstream inputFile("Clients.txt");
+        string line;
+        while (getline(inputFile, line))
+        {
+        // Parse line to Client
+        Client client = Parser::parseToClient(line);
+        cout << "<----------------- Client ------------------------>"<<endl;
+        cout << "Your Id is : "  << client.getId() << endl;
+		cout << "Your Name is :  " << client.getName() << endl;
+		cout << "Your Password is : " << client.getPassword()<< endl;
+		cout << "Your Balance is : " << client.getBalance()<< endl;
+		}
+
+        inputFile.close();
+    }
+
+   static void getEmployees() {
+        ifstream inputFile("Employees.txt");
+        string line;
+        while (getline(inputFile, line))
+        {
+        // Parse line to Client
+        Employee employees = Parser::parseToEmployee(line);
+        cout << "<----------------- Employee ----------------------->"<<endl;
+        cout << "Your Id is : "  << employees.getId() << endl;
+		cout << "Your Name is :  " << employees.getName() << endl;
+		cout << "Your Password is : " << employees.getPassword()<< endl;
+		cout << "Your Salary is : " << employees.getSalary()<< endl;
+		}
+
+    inputFile.close();
+
+   };
+   static void getAdmins(){
+        ifstream inputFile("Admin.txt");
+        string line;
+        while (getline(inputFile, line))
+        {
+        // Parse line to Client
+        Admin admin = Parser::parseToAdmin(line);
+        cout << "<----------------- Admin ----------------------->"<<endl;
+        cout << "Your Id is : "  << admin.getId() << endl;
+		cout << "Your Name is :  " << admin.getName() << endl;
+		cout << "Your Password is : " << admin.getPassword()<< endl;
+		cout << "Your Balance is : " << admin.getSalary()<< endl;
+		}
+
+    inputFile.close();
+   };
+// i will check on it this -> Method!
+   static void clearFile(string fileName, string lastIdFile){
+       ofstream file(fileName);
+       ofstream lastIdFileStream(lastIdFile);
+        if (!file.is_open() || !lastIdFileStream.is_open()) {
+            cerr << "Failed to open file or lastIdFile." << std::endl;
+            return;
+        }
+
+        file.close();
+        lastIdFileStream.close();
+
+        cout << "File cleared successfully." << std::endl;
+   };
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 //classes
-#include<Person.h>
+#include <Person.h>
 #include <Client.h>
 #include <Employee.h>
 #include <Admin.h>
 #include <Validation.h>
-
+#include <FilesHelper.h>
+#include <parser.h>
 //end classes
 //Libarly
 #include <iostream>
@@ -14,44 +15,58 @@
 //end Libarly
 using namespace std;
 
-int main()
-{
-  //  Admin a(1,"maximus","1234",5000); a.display();
+int main() {
 
-      ofstream MyWriteClient("Clients.txt");
-      ofstream MyWriteEmployee("Employee.txt");
-      ofstream MyWriteAdmin("Admin.txt");
-      MyWriteClient << "MyClient*test";
-      MyWriteEmployee << "MyEmployee*test";
-      MyWriteAdmin << "MyAdmin*test";
+      Client a(1,"maximus","123312344",5000)
+            ,b(1,"maximus","12341234",5000)
+            ,c1(1,"MostafaElAraqy","14221234",6000)
+            ,d(1,"Raina","34231234",5600)
+            ,f(1,"Mohamed","1423321234",6000);
 
-      MyWriteClient.close();
-      MyWriteEmployee.close();
-      MyWriteAdmin.close();
-
-      string myText;
-      ifstream MyReadClient("Clients.txt");
-      ifstream MyReadEmployee("Employee.txt");
-      ifstream MyReadAdmin("Admin.txt");
-      while(getline (MyReadClient, myText)){
-        cout << myText;
-      }
-      cout<<endl;
-      while(getline (MyReadEmployee, myText)){
-        cout << myText;
-      }
-      cout<<endl;
-      while(getline (MyReadAdmin, myText)){
-        cout << myText;
-      }
+      Employee v(1,"qwerrqwer","123456793",5000),
+      c(1,"Titfllass","123456793",5000);
+      //this is creation files
+      ofstream MyClient("Clients.txt");
+      ofstream MyEmployee("Employee.txt");
+      ofstream MyAdmin("Admin.txt");
+      ofstream MyClientLastId("ClientLastId.txt");
+      ofstream MyEmployeeLastId("EmployeeLastId.txt");
+      ofstream MyAdminLastId("AdminLastId.txt");
+      string fileName = "ClientLastId.txt";
 
 
 
 
+      FilesHelper::saveLast(fileName,1);
+
+      FilesHelper::saveClient(a);
+      FilesHelper::saveClient(b);
+
+      string fileLAstIdEmp = "EmployeeLastId.txt";
+      string fileEmployee = "Employee.txt";
+      FilesHelper::saveEmployeeOrAdmin(fileEmployee,fileLAstIdEmp,v);
+      FilesHelper::saveEmployeeOrAdmin(fileEmployee,fileLAstIdEmp,c);
+
+      MyClient.close();
+      MyEmployee.close();
+      MyAdmin.close();
+      MyClientLastId.close();
+      MyEmployeeLastId.close();
+      MyAdminLastId.close();
+
+        FilesHelper::saveClient(a);
+        FilesHelper::saveClient(b);
+        FilesHelper::saveClient(c1);
+        FilesHelper::saveClient(d);
+        FilesHelper::saveClient(f);
+
+      FilesHelper::getClients();
+      FilesHelper::getAdmins();
+      FilesHelper::getEmployees();
 
 
    // cout << "Team hug for bugs graduate project..!" << endl;
 
 
   return 0;
-}
+};
